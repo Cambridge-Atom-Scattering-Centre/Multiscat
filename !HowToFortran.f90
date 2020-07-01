076358733fa3350484b7a67aef8c60caad3b3c50
Francesco Bello <FrancescoBello98@live.it>
2020-07-01 11:51:46 +0100

    Added an example of wrting to a variale in !HowToFortran.f90.

--- !HowToFortran.f90
+++ !HowToFortran.f90
@@ -11,6 +11,7 @@
   REAL   TWO,TIME
   real x, y
   integer n
+  character*40 var
 
   ! How to output with print, format is a mess
 
@@ -39,5 +40,15 @@
   ! See http://www.personal.psu.edu/jhm/f90/lectures/23.html
   ! for format specification
   ! FYI: format only works in .f90 files (at least with this syntax)
-
+  
+  !(2) -Francesco
+  !Assign a string variable and format at the same time. This may be the 
+  !only way to assign string varibles from o=numerical types (?)
+  write(var,101)  n
+101 format( 'var has been assigned this string, n= ', i2.0)
+  print *, var
+  ! The write command with the first argument set to a variable writes the result
+  ! to the variable instead of printing to the console. The code is otherwise similar
+  ! to (1). The print statement prints to console so the result of the assignment 
+  ! can be seen
   end
